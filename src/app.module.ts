bf596632024b16ec3ad48ae29e7efc4dd7c09219
adamo jordan <adamo.figueroa@gmail.com>
2025-01-17 02:12:26 -0600

    Adding conversation cards

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { ConfigModule, ConfigType } from '@nestjs/config';
 import envVariables from './config/environment';
 import { InitModule } from './init/init.module';
 import { UserModule } from './user/user.module';
-import { ConversationAiModule } from './conversation-ai/conversation-ai.module';
+// import { ConversationAiModule } from './conversation-ai/conversation-ai.module';
 import { TestModule } from './test/test.module';
 import { AdminModule } from './admin/admin.module';
 
+// import { ConversationCardsModule } from '@dataclouder/conversation-card-nestjs/dist/libs/conversation-cards/src/conversation-cards.module';
+
+import { ConversationCardsModule } from '@dataclouder/conversation-card-nestjs';
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [envVariables], isGlobal: true }),
@@ -19,16 +22,16 @@ import { AdminModule } from './admin/admin.module';
         const { dbName, host, password, user } = configService.mongo;
         const str_con_qa = `mongodb+srv://${user}:${password}@${host}/${dbName}?retryWrites=true&w=majority`;
         console.log('Mongo connectiong with', host);
-        return { uri: str_con_qa, user: '', pass: '', dbName: '' };
+        return { uri: str_con_qa };
       },
       inject: [envVariables.KEY],
     }),
 
     InitModule,
     UserModule,
-    ConversationAiModule,
     TestModule,
     AdminModule,
+    ConversationCardsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/conversation-ai/readme.md
+++
@@ -1,7 +0,0 @@
-### this module needs some dependencies
-
-if you delete the module remember to delete dependencies.
-
-- Conection to database, will create table called conversations
-
-- TTS: this is highly attached to google cloud, so will need some extra dependencies.
